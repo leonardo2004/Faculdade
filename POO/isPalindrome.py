7ca5a88a8e9f2c6e5783501f6f8b7dff9cb964d4
leonardo2004 <dminer3x@gmail.com>
2026-09-02 23:23:16 -0300

    2026

--- POO/isPalindrome.py
+++
@@ -1,9 +0,0 @@
-# Nome: Leonardo Tomasela Leal 
-# RA: 170291
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        a = str(x)
-        b = a[::-1]
-        if a == b:
-            return True
-        return False
--- POO/validAnagram.py
+++
@@ -1,6 +0,0 @@
-#Nome: Leonardo Tomasela Leal RA:170291
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        letras_s = {letra: s.count(letra) for letra in set(s)}
-        letras_t = {letra: t.count(letra) for letra in set(t)}
-        return letras_s == letras_t
